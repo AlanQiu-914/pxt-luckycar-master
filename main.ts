eca56ad102d56e08850ef8913cb4a37e04a0c9ed
AlanQiu-914 <490753458@qq.com>
2021-12-16 15:58:15 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1063,12 +1063,13 @@ namespace luckycar {
          * @param pin the pin where the neopixel is connected.
          * @param numleds number of leds in the strip, eg: 24,30,60,64
          */
-        //% subcategory="RGB_CTR"
-        //% blockId="neopixel_create" block="NeoPixel at pin %pin|with %numleds|leds as %mode"
-        //% weight=90 blockGap=8
-        //% parts="neopixel"
-        //% trackArgs=0,2
-        //% blockSetVariable=strip
+        // subcategory="RGB_CTR"
+        // blockId="neopixel_create" block="NeoPixel at pin %pin|with %numleds|leds as %mode"
+        // weight=90 blockGap=8
+        // parts="neopixel"
+        // trackArgs=0,2
+        // blockSetVariable=strip
+        /*
         export function create(pin: DigitalPin, numleds: number, mode: NeoPixelMode): Strip {
             let strip = new Strip();
             let stride = mode === NeoPixelMode.RGBW ? 4 : 3;
@@ -1081,6 +1082,7 @@ namespace luckycar {
             strip.setPin(pin)
             return strip;
         }
+        */
         /**
          * rgb init DigitalPin.P0, 4 leds,NeoPixelMode.RGB
          */
