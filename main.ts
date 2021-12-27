683c0bcf7efe44a8b8aaed6a02ce3054e36e0a90
AlanQiu-914 <490753458@qq.com>
2021-12-27 15:00:29 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -95,7 +95,7 @@ const enum IrProtocol {
     //% block="NEC"
     NEC = 1,
 }
-//% weight=50 color=#e7660b icon="\uf1b9" block="AlphaCar" block.loc.zh-cn="阿尔法"
+//% weight=50 color=#e7660b icon="\uf1b9" block="AlphaCar" block.loc.zh-cn="阿尔法小车"
 namespace AlphaCar {
     /**
      * 小车马达、循迹控制
@@ -240,7 +240,7 @@ namespace AlphaCar {
      * @param lspeed Left wheel speed , eg: 100
      * @param rspeed Right wheel speed, eg: -100
      */
-    //% subcategory="Motors"
+    //% subcategory="Motors" subcategory.loc.zh-cn="马达"
     //% block="Set left wheel speed %lspeed\\% |right wheel speed %rspeed\\%"
     //% block.loc.zh-cn="设置左侧车轮速度 %lspeed\\% | 右侧车轮速度 %rspeed\\%"
     //% lspeed.min=-100 lspeed.max=100
