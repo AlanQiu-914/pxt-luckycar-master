7b8bca5a5df1f38d4b105cf925079c4b6fb0ddb9
AlanQiu-914 <490753458@qq.com>
2021-12-27 14:54:04 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -96,7 +96,7 @@ const enum IrProtocol {
     NEC = 1,
 }
 //% weight=50 color=#e7660b icon="\uf1b9" block="AlphaCar"
-//% Category.loc.zh-cn="阿尔法小车"
+//% namespace.loc.zh-cn="阿尔法小车"
 namespace AlphaCar {
     /**
      * 小车马达、循迹控制
